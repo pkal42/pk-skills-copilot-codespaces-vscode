1cf525b09e273ea6720a95b75750f862433e6903
Priti Kalani <36218586+pkal42@users.noreply.github.com>
2023-08-11 11:24:25 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,7 +1,14 @@
-function skillsMember() {
-    // Path: member.js
-    this.skills = [];
-    this.addSkill = function (skill) {
-        this.skills.push(skill);
-    };
+function skillsMembers() {
+    var skills = document.getElementById("skills");
+    var members = document.getElementById("members");
+    var skillsMembers = document.getElementById("skillsMembers");
+    var membersSkills = document.getElementById("membersSkills");
+    var skillsMembersButton = document.getElementById("skillsMembersButton");
+    var membersSkillsButton = document.getElementById("membersSkillsButton");
+    skills.style.display = "block";
+    members.style.display = "none";
+    skillsMembers.style.display = "block";
+    membersSkills.style.display = "none";
+    skillsMembersButton.style.backgroundColor = "#f1f1f1";
+    membersSkillsButton.style.backgroundColor = "#fff";
 }
